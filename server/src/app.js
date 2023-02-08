d50d3b40c970fff7a03da405a9b6a421c9fad9cb
Abhishek Silawat <absilawat@gmail.com>
2023-02-08 11:42:24 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,9 @@
 const express = require("express");
-
-// const {register,login} = require("./controllers/auth_controller")
-
-// const postController = require("./controllers/post_controller")
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(express.json());
-
-// app.post("/register",register);
-// app.post("/login",login)
-
-// app.use("/posts",postController)
+app.use("/api", authRoutes);
 
 module.exports = app;
