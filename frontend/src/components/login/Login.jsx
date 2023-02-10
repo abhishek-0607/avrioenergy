2609892d64d105a782ae5a7da216c7e51f7c8cd3
Abhishek Silawat <absilawat@gmail.com>
2023-02-10 18:24:18 +0530

    Login page

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,7 +1,77 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
+import "./login.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  const { setToken } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const initState = {
+    email: "",
+    password: "",
+  };
+  const [user, setUser] = useState(initState);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prev) => ({ ...prev, [name]: value }));
+  };
+  const login = async () => {
+    const res = await fetch("http://localhost:3000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    if (res.status === 201) {
+      const data = await res.json();
+      setToken(data.token);
+
+      console.log("data:", data);
+      setUser({ ...initState });
+      navigate("/users");
+    } else {
+      console.log(res);
+      const err = await res.json();
+      alert(err.message);
+    }
+  };
+  return (
+    <div className="login">
+      <div className="container">
+        <h2>Login</h2>
+        <input
+          onChange={handleChange}
+          type="email"
+          className="inputbox"
+          placeholder="Enter the Email"
+          name="email"
+          value={user.email}
+        />
+
+        <input
+          onChange={handleChange}
+          type="password"
+          className="inputbox"
+          placeholder="Enter the Password"
+          name="password"
+          value={user.password}
+        />
+        <button
+          className="button"
+          onClick={() => {
+            console.log(user);
+            login();
+          }}
+        >
+          Login
+        </button>
+        <p>
+          If you're a new User then <Link to="/register">Register</Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
