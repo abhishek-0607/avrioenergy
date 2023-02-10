a43f2de4878c82373cecfee18bb891f5e09b59ff
Abhishek Silawat <absilawat@gmail.com>
2023-02-10 18:24:49 +0530

    Update Users.jsx

--- frontend/src/components/users/Users.jsx
+++ frontend/src/components/users/Users.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./users.css";
 
 const Users = () => {
-  return <div>Users</div>;
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/users")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("data", data);
+        setUsers(data.users);
+      });
+  }, []);
+
+  return (
+    <div className="users">
+      <div className="tableBox">
+        <table className="table table-hover ">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Contact</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.number}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Users;
